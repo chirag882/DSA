e21cb7caf5b4968e11f063fb4dd46427b5a9ce24
Chirag Ailani <chiragailani35@gmail.com>
2023-05-19 14:10:46 +0530

    graph_1

---
+++ graph/q1.cpp
@@ -0,0 +1,47 @@
+// Question : https://leetcode.com/problems/number-of-provinces/description/
+
+// Solution
+class Solution
+{
+public:
+    void dfs(int node, vector<int> &vis, vector<int> adj[])
+    {
+        vis[node] = 1;
+        for (auto it : adj[node])
+        {
+            if (!vis[it])
+            {
+                dfs(it, vis, adj);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>> &isConnected)
+    {
+        int n = isConnected.size();
+        vector<int> adj[n];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == j)
+                    continue;
+                if (isConnected[i][j])
+                {
+                    adj[i].push_back(j);
+                }
+            }
+        }
+        vector<int> vis(n, 0);
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (!vis[i])
+            {
+                dfs(i, vis, adj);
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
