9cb8ffe5584023dfbdea9202b74960ab2b17764d
Chirag Ailani <chiragailani35@gmail.com>
2023-06-15 00:11:04 +0530

    Time: 19 ms (39.95%), Space: 13.6 MB (8.58%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> temp(n,vector<int>(m,0)); 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j] == 0){
+                    for(int k=0;k<n;k++){
+                        temp[k][j] = -1;
+                    }
+                    for(int k=0;k<m;k++){
+                        temp[i][k] = -1;
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(temp[i][j] == -1){
+                    mat[i][j] = 0;
+                }
+            }
+        }
+    }
+};
