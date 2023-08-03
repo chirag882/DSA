44dcbc7746a501cb161b8b8f40883e37abc2cebd
Chirag Ailani <chiragailani35@gmail.com>
2023-08-03 12:14:52 +0530

    Time: 205 ms (63.73%), Space: 117.5 MB (7.84%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -22,16 +22,12 @@ public:
         st[idx] = merge(st[idx<<1],st[idx<<1|1]);
     }
     int query(int idx,int l,int r,int ql,int qr){
-        // cout<<r<<"--\n";
         if(r < ql || l > qr){
-            // cout<<r<<"--\n";
-            // cout<<"h0"<<"\n";
             return -1;
         }
         if(ql <= l && r <= qr){
             return st[idx];
         }
-        // cout<<l<<" "<<r<<"\n";
         int mid = (l+r)/2;
         int left = query(idx<<1,l,mid,ql,qr);
         int right = query(idx<<1|1,mid+1,r,ql,qr);
@@ -40,10 +36,7 @@ public:
     int rec(int l,int r,vector<int> &v){
         if (l > r)  return INT_MIN;
         if (l == r)  return v[l];
-        // cout<<r<<"--\n";
-        // cout<<n-1<<"\n";
         int idx = query(1,0,n-1,l,r);
-        // cout<<idx<<"\n";
         if(idx != -1){
             return max({rec(l,idx-1,v),rec(idx+1,r,v),v[idx]*(r-l+1)});
         }
@@ -51,12 +44,8 @@ public:
     }
     int largestRectangleArea(vector<int>& h) {
         n = h.size();
-        v.resize(n);
-        for(int i=0;i<n;i++){
-            v[i] = h[i];
-        }
-        int size = 4*n;
-        st.resize(size);
+        v = h;
+        st.resize(4*n);
         build(1,0,n-1);
         return rec(0,n-1,h);
     }
