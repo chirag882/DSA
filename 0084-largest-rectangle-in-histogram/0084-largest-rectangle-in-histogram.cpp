9ee3a6a4558e4d8bf424bb054e202a7d255921f1
Chirag Ailani <chiragailani35@gmail.com>
2023-08-03 12:13:41 +0530

    Time: 206 ms (63.02%), Space: 117.4 MB (7.84%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,28 +1,63 @@
 class Solution {
 public:
+    vector<int> st;
+    vector<int> v;
+    int n;
+    int merge(int i,int j){
+        if(i==-1)return j;
+        if(j==-1)return i;
+        if(v[i] < v[j]){
+            return i;
+        }
+        return j;
+    }
+    void build(int idx,int l,int r){
+        if(l == r){
+            st[idx] = l;
+            return;
+        }
+        int mid = (l+r)/2;
+        build(idx<<1,l,mid);
+        build(idx<<1|1,mid+1,r);
+        st[idx] = merge(st[idx<<1],st[idx<<1|1]);
+    }
+    int query(int idx,int l,int r,int ql,int qr){
+        // cout<<r<<"--\n";
+        if(r < ql || l > qr){
+            // cout<<r<<"--\n";
+            // cout<<"h0"<<"\n";
+            return -1;
+        }
+        if(ql <= l && r <= qr){
+            return st[idx];
+        }
+        // cout<<l<<" "<<r<<"\n";
+        int mid = (l+r)/2;
+        int left = query(idx<<1,l,mid,ql,qr);
+        int right = query(idx<<1|1,mid+1,r,ql,qr);
+        return merge(left,right);
+    }
+    int rec(int l,int r,vector<int> &v){
+        if (l > r)  return INT_MIN;
+        if (l == r)  return v[l];
+        // cout<<r<<"--\n";
+        // cout<<n-1<<"\n";
+        int idx = query(1,0,n-1,l,r);
+        // cout<<idx<<"\n";
+        if(idx != -1){
+            return max({rec(l,idx-1,v),rec(idx+1,r,v),v[idx]*(r-l+1)});
+        }
+        return INT_MIN;
+    }
     int largestRectangleArea(vector<int>& h) {
-        int n = h.size();
-        stack<int> st;
-        int ans = 0;
-        int i;
-        for(i=0;i<n;i++){
-            if(st.empty() || h[i] >= h[st.top()]){
-                st.push(i);
-            }else{
-                while(!st.empty() && h[i] < h[st.top()]){
-                    int ht = h[st.top()];
-                    st.pop();
-                    ans = max(ans,ht*(st.empty() ? (i) : i-st.top()-1));
-                }
-                st.push(i);
-            }
+        n = h.size();
+        v.resize(n);
+        for(int i=0;i<n;i++){
+            v[i] = h[i];
         }
-        while(!st.empty()){
-            int ht = h[st.top()];
-            st.pop();
-            ans = max(ans,ht*(st.empty() ? (i) : i-st.top()-1));
-            cout<<ans<<"\n";
-        }
-        return ans;
+        int size = 4*n;
+        st.resize(size);
+        build(1,0,n-1);
+        return rec(0,n-1,h);
     }
 };
