4a99362d83bd06f3d3a5d8130613f7fe195bc774
Chirag Ailani <chiragailani35@gmail.com>
2023-08-21 16:10:03 +0530

    Time: 7 ms (96.4%), Space: 8.8 MB (63.72%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,52 +1,57 @@
-class Solution {
-public:
-    string longestPalindrome(string s) {
-        string temp = "";
-        for(auto it : s){
-            temp += '#';
-            temp += it;
+class Manacher{
+private:
+    string updatedString(string s){
+        string temp = "@";
+        for(int i=0;i<s.size();i++){
+            temp += "#";
+            temp += s[i];
         }
-        temp += '#';
-        temp = '$' + temp;
-        temp = temp + '*';
+        temp += "#";
+        temp += "*";
+        return temp;
+    }
+    vector<int> LPS(string s){
+        string temp = updatedString(s);
         int n = temp.size();
         vector<int> lps(n,0);
+        int c=0,r=0;
         for(int i=1;i<n-1;i++){
-            while(temp[i+1+lps[i]] == temp[i-1-lps[i]]){
+            int mirror = c-(i-c);
+            if(i<r){
+                lps[i]=min(lps[mirror],r-i);
+            }
+            while(temp[i + 1 + lps[i]] == temp[i - 1 - lps[i]]){
                 lps[i]++;
             }
+            if(i+lps[i]>r){
+                r = i+lps[i];
+                c = i;
+            }
         }
-        int maxi = INT_MIN;
+        return lps;
+    }
+public:
+    string longestPalindromeSubstring(string s){
+        vector<int> lps = LPS(s);
         int idx;
-        for(int i=0;i<n;i++){
+        int maxi = INT_MIN;
+        for(int i=1;i<lps.size()-1;i++){
             if(maxi < lps[i]){
                 maxi = lps[i];
                 idx = i;
             }
         }
-        cout<<temp<<"\n";
-        cout<<maxi<<" "<<idx<<"\n";
-        string ans = "";
-        if(maxi % 2){
-            int sz = 0;
-            while(sz < maxi){
-                ans = ans + temp[idx+sz];
-                sz+=2;
-            }
-            string temp = ans.substr(1);
-            reverse(temp.begin(),temp.end());
-            ans = temp + ans;
-        }else{
-            int sz = 1;
-            while(sz < maxi){
-                ans = ans + temp[idx+sz];
-                sz+=2;
-            }
-            string temp = ans;
-            reverse(temp.begin(),temp.end());
-            ans = temp + ans;
-        }
-        
+        int startpoint = idx - maxi + 1 ;
+        int actualidx = (startpoint - 2)/2;
+        string ans = s.substr(actualidx,maxi);
         return ans;
+    }
+};
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        Manacher obj1;
+        return obj1.longestPalindromeSubstring(s);
     }
 };
