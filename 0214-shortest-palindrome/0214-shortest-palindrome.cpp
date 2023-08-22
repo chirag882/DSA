938809187f249e4c9a1a85e43fedf3094dae8531
Chirag Ailani <chiragailani35@gmail.com>
2023-08-22 12:13:44 +0530

    Time: 14 ms (13.96%), Space: 10.7 MB (7.1%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,29 +1,90 @@
-class Solution {
-public:
-    int lps(string temp){
+class Manacher{
+private:
+    string updatedString(string s){
+        string temp = "@";
+        for(int i=0;i<s.size();i++){
+            temp += "#";
+            temp += s[i];
+        }
+        temp += "#";
+        temp += "*";
+        return temp;
+    }
+    vector<int> LPS(string s){
+        string temp = updatedString(s);
         int n = temp.size();
         vector<int> lps(n,0);
-        for(int i=1;i<n;i++){
-            int j = lps[i-1];
-            while(j > 0 && temp[i] != temp[j]){
-                j = lps[j-1];
+        int c=0,r=0;
+        for(int i=1;i<n-1;i++){
+            int mirror = c-(i-c);
+            if(i<r){
+                lps[i]=min(lps[mirror],r-i);
             }
-            if(temp[i] == temp[j]){
-                j++;
-                lps[i] = j;
+            while(temp[i + 1 + lps[i]] == temp[i - 1 - lps[i]]){
+                lps[i]++;
+            }
+            if(i+lps[i]>r){
+                r = i+lps[i];
+                c = i;
             }
         }
-        return lps[temp.size()-1];
+        return lps;
     }
+public:
+    vector<int> generatelps(string s){
+        vector<int> lps = LPS(s);
+        return lps;
+    }
+    void longestPalindromeSubstring(string s,vector<int> &lps){
+        int idx;
+        int maxi = INT_MIN;
+        for(int i=1;i<lps.size()-1;i++){
+            if(maxi < lps[i]){
+                maxi = lps[i];
+                idx = i;
+            }
+        }
+        int startpoint = idx - maxi + 1 ;
+        int actualidx = (startpoint - 2)/2;
+        string ans = s.substr(actualidx,maxi);
+        cout<<ans<<"\n";
+    }
+    bool checkPalindrome(int l,int r,vector<int> &lps){
+        int len = r-l+1;
+        int mid;
+        int lpsindex;
+        if(len % 2){
+            mid = (l+r)/2;
+            lpsindex = 2*mid+2;
+        }else{
+            mid = (l+r)/2;
+            lpsindex = 2*mid+3;
+        }
+        if(len <= lps[lpsindex]){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
+class Solution {
+public:
     string shortestPalindrome(string s) {
-        string rev = s;
-        reverse(rev.begin(),rev.end());
-        string temp = s + '#' + rev; 
-        int j = lps(temp);
-        string u = s.substr(j);
-        // cout<<u<<"\n";
+        if(s.empty()){
+            return s;
+        }
+        Manacher obj1;
+        vector<int> lps = obj1.generatelps(s);   
+        int idx;
+        int n = s.size();
+        for(int i=0;i<n;i++){
+            if(obj1.checkPalindrome(0,i,lps)){
+                idx = i;
+            }
+        }
+        string u = s.substr(idx+1);
         reverse(u.begin(),u.end());
-        s = u + s;
+        s = u+s;
         return s;
     }
 };
